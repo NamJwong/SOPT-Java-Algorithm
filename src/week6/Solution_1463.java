bf825d748fefcd385e1de581f3d7a1c8839b3ae1
Chan <ieejo716@naver.com>
2021-11-24 16:49:33 +0900

    [S1] 1로 만들기

--- src/week6/Solution_1463.java
+++ src/week6/Solution_1463.java
@@ -9,28 +9,29 @@ public class Solution_1463 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
 
-        dp[1] = 0;
+        //이미 1
+        arr[1] = 0;
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;  // 3번째 연산
-            System.out.println("dp[" + i + "] = dp[" + (i - 1) + "] + 1");
-            System.out.println("dp[" + i + "] = " + dp[i]);
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
+            System.out.println("arr[" + i + "] = arr[" + (i - 1) + "] + 1");
+            System.out.println("arr[" + i + "] = " + arr[i]);
             if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 2번째 연산
-                System.out.println("dp[" + i + "] = dp[" + (i - 1) + "] + 1");
-                System.out.println("dp[" + i + "] = " + dp[i]);
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+                System.out.println("arr[" + i + "] = arr[" + (i - 1) + "] + 1");
+                System.out.println("arr[" + i + "] = " + arr[i]);
             }
             if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 1번째 연산
-                System.out.println("dp[" + i + "] = dp[" + (i - 1) + "] + 1");
-                System.out.println("dp[" + i + "] = " + dp[i]);
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+                System.out.println("arr[" + i + "] = arr[" + (i - 1) + "] + 1");
+                System.out.println("arr[" + i + "] = " + arr[i]);
             }
         }
 
-        bw.write(dp[N] + "\n");
+        bw.write(arr[n] + "\n");
 
         br.close();
         bw.flush();

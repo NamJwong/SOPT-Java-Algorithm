83c3357ef6912a3044cdf0115a45f69a5691a0bb
남주영 <73823388+NamJwong@users.noreply.github.com>
2022-02-06 17:11:26 +0900

    1026번 보물

---
+++ 2-Juyeong-Nam/Week2_1026/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        Integer[] B = new Integer[N];
+        for(int i = 0; i < N; i++) {
+            B[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+        int answer = 0;
+        for(int i = 0; i < N; i++) {
+            answer += A[i]*B[i];
+        }
+        System.out.println(answer);
+    }
+}

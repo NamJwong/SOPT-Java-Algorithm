092545816d1e446acda451593d4bc7e473ea2a45
남주영 <73823388+NamJwong@users.noreply.github.com>
2022-02-06 17:41:04 +0900

    [문제 풀이 1회차] 단어 뒤집기 2

--- 2-Juyeong-Nam/Week1_17413/Main.java
+++ 2-Juyeong-Nam/Week1_17413/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String S = sc.nextLine();
-        sc.close();
+        sc.close(); 
 
         int start, end;
         for(int i = 0; i < S.length(); i++) {

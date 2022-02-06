dfd872d42fe9811d8f3e626056ce4bbdae5a7f61
남주영 <73823388+NamJwong@users.noreply.github.com>
2022-02-06 17:41:54 +0900

    [문제 풀이 1회차] 스택 수열

--- 2-Juyeong-Nam/Week1_1874/Main.java
+++ 2-Juyeong-Nam/Week1_1874/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int[] arr = new int[n];
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-        }
+        } 
         sc.close();
 
         Stack<Integer> stack = new Stack<>();
